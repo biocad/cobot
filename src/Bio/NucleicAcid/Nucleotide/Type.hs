bd6b514e51c7dd24977c61fe9ece9d2fb80e37c6
Bogdan Neterebskii <bog2dan1@gmail.com>
2019-04-06 16:01:18 +0200

    complementary and reverse complementary added

--- src/Bio/NucleicAcid/Nucleotide/Type.hs
+++ src/Bio/NucleicAcid/Nucleotide/Type.hs
@@ -1,9 +1,22 @@
-module Bio.NucleicAcid.Nucleotide.Type where
+{-# LANGUAGE DeriveAnyClass #-}
 
-import Control.Lens (Iso', iso)
+module Bio.NucleicAcid.Nucleotide.Type
+  ( DNA (..)
+  , RNA (..)
+  , nucleoIso
+  , toRNA
+  , toDNA
+  , Complementary (..)
+  ) where
+
+import           Control.DeepSeq (NFData)
+import           Control.Lens    (Iso', iso)
+import           Data.Array      (Array, Ix, bounds, listArray)
+import           Data.Foldable   (Foldable (..))
+import           GHC.Generics    (Generic)
 
 data DNA = DA | DC | DG | DT
-  deriving (Eq, Ord, Bounded, Enum)
+  deriving (Eq, Ord, Bounded, Enum, Generic, NFData)
 
 instance Show DNA where
     show DA = "Adenine"
@@ -12,7 +25,7 @@ instance Show DNA where
     show DT = "Thymine"
 
 data RNA = RA | RC | RG | RU
-  deriving (Eq, Ord, Bounded, Enum)
+  deriving (Eq, Ord, Bounded, Enum, Generic, NFData)
 
 instance Show RNA where
     show RA = "Adenine"
@@ -20,17 +33,62 @@ instance Show RNA where
     show RG = "Guanine"
     show RU = "Uracil"
 
+-------------------------------------------------------------------------------
+-- Transciption
+-------------------------------------------------------------------------------
+
 nucleoIso :: Iso' DNA RNA
 nucleoIso = iso toRNA toDNA
 
+{-# INLINE toRNA #-}
 toRNA :: DNA -> RNA
 toRNA DA = RA
 toRNA DC = RC
 toRNA DG = RG
 toRNA DT = RU
 
+{-# INLINE toDNA #-}
 toDNA :: RNA -> DNA
 toDNA RA = DA
 toDNA RC = DC
 toDNA RG = DG
 toDNA RU = DT
+
+------------------------------------------------------------------------------
+-- Complementary and reverse complementary
+-------------------------------------------------------------------------------
+
+class Complementary a where
+    -- | complement *NA (DNA or RNA)
+    --
+    cNA :: a -> a
+
+    -- | reverce complement *NA (DNA or RNA)
+    --
+    rcNA :: a -> a
+
+instance Complementary DNA where
+    cNA DA = DT
+    cNA DC = DG
+    cNA DG = DC
+    cNA DT = DA
+
+    rcNA = cNA
+
+instance Complementary RNA where
+    cNA = toRNA . cNA . toDNA
+
+    rcNA = cNA
+
+instance Complementary a => Complementary [a] where
+   cNA = fmap cNA
+
+   rcNA = reverse . cNA
+
+instance (Complementary a, Ix i) => Complementary (Array i a) where
+   cNA = fmap cNA
+
+   rcNA l = listArray (bounds l) rl
+     where
+       rl = rcNA . toList $ l
+
